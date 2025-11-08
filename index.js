8a2183d8b410bce27c5497191dcd00e5296bc941
Ansar Askhat <ansarstellar@gmail.com>
2025-11-09 00:03:18 +0100

    structure routes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import Joi from "joi";
-import { log } from "./logger.js";
+import { log } from "./middleware/logger.js";
 import helmet from "helmet";
 import morgan from "morgan";
+import courses from "./routes/courses.js";
+import home from "./routes/home.js";
+import c from "config";
 dotenv.config();
 const app = express();
 
@@ -16,86 +19,8 @@ app.use(express.static("public"));
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(log);
-
-const courses = [
-    {
-        id: 1,
-        name: "course1",
-    },
-    {
-        id: 2,
-        name: "course2",
-    },
-    {
-        id: 3,
-        name: "course3",
-    },
-];
-app.get("/", (req, res) => {
-    res.render("index", { title: "My Express App", message: "Hello" });
-});
-
-app.get("/api/courses", (req, res) => {
-    res.send(courses);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("Course not found!");
-    res.send(course);
-});
-
-app.post("/api/courses", (req, res) => {
-    const { error } = validateCourse(req.body);
-
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name,
-    };
-
-    courses.push(course);
-    res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("Course with given ID not found!");
-
-    const { error } = validateCourse(req.body);
-
-    if (error) {
-        res.status(400).send(error.details[0].message);
-        return;
-    }
-
-    course["name"] = req.body.name;
-    res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-    const course = courses.find((c) => c.id === parseInt(req.params.id));
-    if (!course) return res.status(404).send("Course with given ID not found!");
-
-    const index = courses.indexOf(course);
-    if (index > -1) {
-        courses.splice(index, 1);
-    }
-
-    res.send(course);
-});
-
-function validateCourse(course) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required(),
-    });
-
-    return schema.validate(course);
-}
+app.use("/api/courses", courses);
+app.use("/", home);
 
 // port
 const port = process.env.PORT || 3000;
---
+++ routes/home.js
@@ -0,0 +1,8 @@
+import express, { json } from "express";
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.render("index", { title: "My Express App", message: "Hello" });
+});
+
+export default router;
